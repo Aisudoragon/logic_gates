ea236a6c38578cca3a4ce75e5c58e1f626537b7e
Aisudoragon <krystiandziura101@gmail.com>
2025-09-18 11:45:38 +0200

    Move menu scenes and add some more code
    Sandbox button in main menu now gives a choice.

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,18 +1,26 @@
-extends VBoxContainer
+extends Control
 
 
 func _on_lessons_button_pressed() -> void:
 	visible = false
 
-	var error: Error = get_tree().change_scene_to_file("res://scenes/level_selection.tscn")
+	var error: Error = get_tree().change_scene_to_file("res://scenes/main_menu/level_selection_menu.tscn")
 	if error:
 		printerr("Couldn't change to Level selection scene")
 
 
 func _on_sandbox_button_pressed() -> void:
+	($SandboxButtons as Control).visible = not ($SandboxButtons as Control).visible
+
+
+func _on_new_board_button_pressed() -> void:
 	var error: Error = get_tree().change_scene_to_file("res://scenes/levels/playground/playground.tscn")
 	if error:
-		printerr("Couldn't change to playground scene")
+		printerr("Couldn't change to sandbox scene")
+
+
+func _on_load_board_button_pressed() -> void:
+	($LoadBoardDialog as FileDialog).visible = not ($LoadBoardDialog as FileDialog).visible
 
 
 func _on_options_button_pressed() -> void:
