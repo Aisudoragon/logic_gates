6fbcc4152c876227bd049bf048891a061aa936dc
Aisudoragon <krystiandziura101@gmail.com>
2025-10-22 11:02:49 +0200

    Fix going back to main menu and loading another file

--- main.gd
+++ main.gd
@@ -21,9 +21,13 @@ func _change_scene(new_scene: String) -> void:
 	if new_node.has_signal(&"change_scene"):
 		var error: Error = new_node.connect(&"change_scene", _on_main_menu_change_scene)
 		if error:
-			print('Something went wrong with connecting "change_scene" signal')
+			printerr('[%d] Something went wrong with connecting "change_scene" signal' % error)
 	if loaded_file_path and new_scene == "playground":
 		current_scene.propagade_file_path(loaded_file_path)
+	if current_scene.has_signal(&"load_file"):
+		var error: Error = current_scene.load_file.connect(_on_main_menu_load_file)
+		if error:
+			printerr('[%d] Something went wrong with connecting to "load_file" signal' % error)
 
 
 func _on_main_menu_change_scene(new_scene: String) -> void:
