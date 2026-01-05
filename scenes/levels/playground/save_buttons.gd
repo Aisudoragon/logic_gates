347acad935dc5fc43b0b2e23aa5e0d522031700e
Aisudoragon <krystiandziura101@gmail.com>
2026-01-05 06:18:12 +0100

    Fix crash while clicking "save"

--- scenes/levels/playground/save_buttons.gd
+++ scenes/levels/playground/save_buttons.gd
@@ -6,10 +6,10 @@ var save_path: String
 
 func _on_save_button_pressed() -> void:
 	if not save_path.is_empty():
-		var error := ($FileDialog as FileDialog).emit_signal(&"file_selected", save_path)
+		var error := ($SaveDialog as FileDialog).emit_signal(&"file_selected", save_path)
 		assert(error == OK, "Couldn't emit signal")
 		return
-	($FileDialog as FileDialog).visible = not ($FileDialog as FileDialog).visible
+	($SaveDialog as FileDialog).visible = not ($SaveDialog as FileDialog).visible
 
 
 func _on_file_dialog_file_selected(path: String) -> void:
